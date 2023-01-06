f2df3e7a0adde3fecd51404f9a91a8c1f1c3ce88
IcaroManuel <icaroomanuel@gmail.com>
2023-01-06 10:44:49 -0300

    changes

--- src/modules/appointments/infra/http/controllers/ProvidersControllers.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersControllers.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 import { container } from 'tsyringe'
 import { instanceToPlain } from 'class-transformer'
 
-import ListProviderService from '@modules/appointments/services/ListProviderService'
+import ListProviderService from '@modules/appointments/services/ListProvidersService'
 
 export default class ProvidersControllers {
   public async index(request: Request, response: Response): Promise<Response> {
